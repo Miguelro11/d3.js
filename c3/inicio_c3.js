eab36558e971b42146e519bb8f4b8dc983387d7a
Miguelro11 <frrgcwp@gmail.com>
2018-01-07 13:05:24 +0100

    Create inicio_c3.js

---
+++ c3/inicio_c3.js
@@ -0,0 +1,4 @@
+<meta charset="utf-8" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.10/c3.min.css" rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.6/d3.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.10/c3.min.js"></script>
